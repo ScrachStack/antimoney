b06775030ee954feaad7b3a5a43e372c61448545
ScratchStack <122732007+ScrachStack@users.noreply.github.com>
2025-11-23 18:12:11 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,8 +6,12 @@ cfg.max = {
     ['black_money'] = 500000
 }
 
+cfg.esxResource = 'es_extended'   -- default ESX resource name
+cfg.qbResource  = 'qb-core'       -- default QB-Core resource name
+
 cfg.whitelisted_groups = { 'admin' } -- Whitelisted group(s)
 cfg.flag = 'kick' -- kick, wipe, log (Kick = Kick & Wipes players money | Wipe = Only wipe players money | Log = Only log to discord [cfg.webhook must be filled out]) [Both Kick & Wipe log if you have it filled out below]
 
-cfg.checkTime = 15 -- Seconds
-cfg.webhook = '' -- The webhook the logs are sent to
+cfg.checkTime = 15
+
+cfg.webhook = 'WEBHOOK_HERE'
